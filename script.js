a426c8e295976650f7697b6200ad2b8188b01a22
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-04-22 18:19:46 -0500

    create progress bar and timestamping

--- script.js
+++ script.js
@@ -23,11 +23,25 @@ function updatePlayIcon(){
 
 
 function updateProgress(){
-  return true
+  progress.value = (video.currentTime / video.duration) * 100;
+
+
+  let mins = Math.floor(video.currentTime / 60);
+  if(mins < 10){
+    mins = '0' + String(mins)
+  }
+
+ 
+  let secs = Math.floor(video.currentTime % 60);
+  if(secs < 10){
+    secs = '0' + String(secs)
+  }
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 function setVideoProgress(){
-  return true
+  video.currentTime = (+progress.value * video.duration) / 100;
+
 }
 
 function stopVideo(){
